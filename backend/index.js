b8241df75d9a1ab0a906a97da3511fa3cf626f64
ogXmessi <142476850+omkarlovesdebugging@users.noreply.github.com>
2025-02-11 20:17:40 +0530

    added cors package

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,22 @@ const cors = require('cors')
 
 mongoDB();
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-allow-Origin","https://mernapp-frontend-1ocp.onrender.com");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-allow-Origin","https://mernapp-frontend-1ocp.onrender.com");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin,X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// })
+
+//cors properly
+
+app.use(cors({
+  origin: "https://mernapp-frontend-1ocp.onrender.com",  // Allow frontend URL
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+}));
 
 //middleware
 app.use(express.json())
